59bf932327fbd5a0fa1680f83056f77cbd96d8c8
michael hohl <mhohl@rocketmail.com>
2018-05-02 23:06:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,11 +67,11 @@ class Dog
   end
 
   def update
-  sql = <<-SQL
-    UPDATE dogs
-    SET name = ?, grade = ?
-    WHERE id = ?
-    SQL
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?, breed = ?
+      WHERE id = ?
+      SQL
 
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
