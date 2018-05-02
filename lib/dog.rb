490c201bc50ee98af459836611ae66a9a735c768
michael hohl <mhohl@rocketmail.com>
2018-05-02 23:19:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,9 +55,9 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).flatten
-    if !dog.empty?
-      self.new_from_db(dog)
+    row = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).flatten
+    if !row.empty?
+      self.new_from_db(row)
     else
       self.create(name: name, breed: breed)
     end
