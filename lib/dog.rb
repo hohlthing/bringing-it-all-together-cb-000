6060a149fe5d54b7f895337aca066bfc434a0196
michael hohl <mhohl@rocketmail.com>
2018-05-02 22:42:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
   attr_accessor :name, :breed
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name:, breed:, id: nil)
     @name = name
@@ -9,7 +9,7 @@ class Dog
     @id = id
   end
 
-  def self.create_table 
+  def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
         id INTEGER PRIMARY KEY,
